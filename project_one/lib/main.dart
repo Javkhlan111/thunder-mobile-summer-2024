3cf53842ca3d4d6f0652c0e496f1e30ffced9875
Thunder Mobile Evening <thunder-01@ADMIN-01s-iMac.local>
2024-07-19 21:27:45 +0800

    add: project_one

--- project_one/lib/main.dart
+++ project_one/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:project_one/pages/page_one.dart';
 import 'package:project_one/widgets/gradient_letter.dart';
 import 'package:project_one/widgets/gradient_text.dart';
 
@@ -6,63 +7,7 @@ void main() {
   runApp(MaterialApp(
     home: Scaffold(
       body: SafeArea(
-        child: Container(
-          decoration: BoxDecoration(
-              image: DecorationImage(
-                 image: AssetImage("assets/images/back1.png"),
-                fit: BoxFit.cover,
-          )
-          ),
-          width: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  GradientLetter("W"),
-                  SizedBox(width: 10,),
-                  GradientLetter("O"),
-                  SizedBox(width: 10,),
-                  GradientLetter("R"),
-                  SizedBox(width: 10,),
-                  GradientLetter("D"),
-                  SizedBox(width: 10,),
-
-
-                ],
-              ),
-              GradientText("Game", 38),
-              Image.asset("assets/images/icodeGuy.png"),
-              SizedBox(height: 20,),
-              GradientText("Ready", 25),
-              SizedBox(height: 20,),
-
-              Container(
-                width: 310,
-                height: 60,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: [Color(0xFFE76A01), Color(0xFFF99440)],
-                    begin: Alignment(1, 0.02),
-                    end: Alignment(-1, -0.02),
-                  ),
-                      borderRadius: BorderRadius.all(Radius.circular(25)),
-                ),
-                  child: TextButton(
-                    onPressed: (){},
-                    child: Text("PLAY", style: TextStyle(fontSize: 24, color: Colors.white),),
-                    style: TextButton.styleFrom(),
-
-                  )
-
-              ),
-
-
-            ],
-
-
-              )),
+        child: PageOne(),
         ),
       ),
     ),
---
+++ project_one/lib/pages/page_one.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/gradient_letter.dart';
+import '../widgets/gradient_text.dart';
+class PageOne extends StatelessWidget {
+  const PageOne({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/back1.png"),
+              fit: BoxFit.cover,
+            )
+        ),
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                GradientLetter("W"),
+                SizedBox(width: 10,),
+                GradientLetter("O"),
+                SizedBox(width: 10,),
+                GradientLetter("R"),
+                SizedBox(width: 10,),
+                GradientLetter("D"),
+                SizedBox(width: 10,),
+
+
+              ],
+            ),
+            GradientText("Game", 38),
+            Image.asset("assets/images/icodeGuy.png"),
+            SizedBox(height: 20,),
+            GradientText("Ready", 25),
+            SizedBox(height: 20,),
+
+            Container(
+                width: 310,
+                height: 60,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Color(0xFFE76A01), Color(0xFFF99440)],
+                    begin: Alignment(1, 0.02),
+                    end: Alignment(-1, -0.02),
+                  ),
+                  borderRadius: BorderRadius.all(Radius.circular(25)),
+                ),
+                child: TextButton(
+                  onPressed: (){},
+                  child: Text("PLAY", style: TextStyle(fontSize: 24, color: Colors.white),),
+                  style: TextButton.styleFrom(),
+
+                )
+
+            ),
+
+
+          ],
+
+
+        ));
+  }
+}
